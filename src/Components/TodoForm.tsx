a66b981fed5bbe4d5e0883dabf630a29934fb42a
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-06-25 22:58:50 +0330

    feat/add todo

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,7 +1,24 @@
-function TodoForm() {
+import { useState } from "react";
+
+type TodoFormProps = {
+  addTodo: (title: string) => boolean;
+};
+
+function TodoForm({ addTodo }: TodoFormProps) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (value) {
+      addTodo(value);
+      setValue("");
+    }
+  };
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={handleSubmit}>
       <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         type="text"
         className="todo-input"
         placeholder="What is the task today?"
--- src/Components/TodosWrapper.tsx
+++ src/Components/TodosWrapper.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
 function TodosWrapper() {
+  const [todos, setTodos] = useState<TTodo[]>([]);
+
+  const addTodo = (title: string) => {
+    setTodos([...todos, { id: crypto.randomUUID(), title, completed: false }]);
+
+    return true;
+  };
+  const deleteTodo = (id: string) => {};
+  const toggleTodo = (id: string) => {};
   return (
     <div className="TodoWrapper">
       <h1>Todo List ❤️ </h1>
       {/* Add New Todo Form */}
-      <TodoForm />
+
+      <TodoForm addTodo={addTodo} />
+
       {/* display todos */}
+      {todos.map((todo) => (
+        <Todo />
+      ))}
+
       <Todo />
     </div>
   );
